6e5cb3e63a465f6bc1c62f523a4c738d25ae1753
wan <wan@wan>
2012-07-25 15:59:18 +0530

    my 2nd  change

--- src/testbuddhika/TestBuddhika.java
+++ src/testbuddhika/TestBuddhika.java
@@ -19,6 +19,8 @@ public class TestBuddhika {
         System.out.println("From Southern PDHS");
         System.out.println("From nimal to Wanasinghe");
         System.out.println("From wan");
+        System.out.println("From wan to nimal  ");
+        
         
     }
 }
